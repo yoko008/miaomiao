ed111aa7cee201dcfef234f5b99de937c85b99e3
bugmaker <289115128@@.com>
2018-11-19 10:40:58 +0800

    主页部分完成

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,30 +1,41 @@
 Page({
   data: {
-    multiArray: [['吃喝', '玩乐'], ['早餐', '午餐']],
-    objectMultiArray: [
-      [
-        {
-          id: 0,
-          name: '吃喝'
-        },
-        {
-          id: 1,
-          name: '玩乐'
-        }
-      ], [
-        {
-          id: 0,
-          name: '早餐'
-        },
-        {
-          id: 1,
-          name: '午餐'
-        }
-      ]
-    ],
+    accountTypeArray:null,
+    multiArray: null,
     multiIndex: [0, 0],
     date: '2018-11-15',
-    time: '12:01'
+    time: '12:01',
+     step: 1,
+    counterId: '',
+    openid: '',
+    count: null,
+    queryResult: [],
+  },
+  //加载页面时触发
+  onLoad: function () {
+
+    const db = wx.cloud.database()
+    // 加载当前用户的记账类型
+    db.collection('account_type').where({
+      _openid: this.data.openid
+    }).get({
+      success: res => {
+        this.setData({
+          accountTypeArray: res.data[0],
+          multiArray: [res.data[0].level1, res.data[0].level2[0]]
+        })
+        console.log('[数据库] [查询记录] 成功: ', res)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
+  },
+  onReady: function () {
   },
   bindMultiPickerColumnChange: function (e) {
     console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
@@ -33,14 +44,69 @@ Page({
       multiIndex: this.data.multiIndex
     };
     data.multiIndex[e.detail.column] = e.detail.value;
-    switch (data.multiIndex[0]) {
-          case 0:
-            data.multiArray[1] = ['早餐', '午餐'];
-            break;
-          case 1:
-            data.multiArray[1] = ['电影', '运动'];
-            break;
-        }
+    if (e.detail.column==0)
+    data.multiArray[1] = this.data.accountTypeArray.level2[e.detail.value];
     this.setData(data);
-  }
+  },
+
+
+  
+
+  onAdd: function () {
+    const db = wx.cloud.database()
+    db.collection('accounts').add({
+      data: {
+        shouzhi:1,
+        jine: 1,
+        beizhu:"买菜的",
+        accountType1:"吃喝",
+        accountType2: "早餐",
+        date:"2018-11-15",
+        time:"12:01",
+        creatTime:new Date(),
+        updateTime:new Date()
+      },
+      success: res => {
+        // 在返回结果中会包含新创建的记录的 _id
+        this.setData({
+          counterId: res._id,
+          count: 1
+        })
+        wx.showToast({
+          title: '保存成功',
+        })
+        console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '新增记录失败'
+        })
+        console.error('[数据库] [新增记录] 失败：', err)
+      }
+    })
+  },
+
+  onQuery: function () {
+    const db = wx.cloud.database()
+    // 查询当前用户所有的 counters
+    db.collection('account_type').where({
+      _openid: this.data.openid
+    }).get({
+      success: res => {
+        this.setData({
+          accountTypeArray: res.data[0],
+          multiArray: [res.data[0].level1, res.data[0].level2[0]]
+        })
+        console.log('[数据库] [查询记录] 成功: ', res)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
+  },
 })
