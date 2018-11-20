9c137da5c55db11baff407edccbb15aedbcf644e
bugmaker <289115128@@.com>
2018-11-20 18:29:54 +0800

    记账页面基础功能完成

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -14,40 +14,33 @@ Page({
     beizhu: "",
     accountType1: "",
     accountType2: "",
-    date: '2018-11-15',
+    date: '',
+    startDate: '',
     time: '12:01',
   },
   //加载页面时触发
   onLoad: function() {
-    const db = wx.cloud.database()
-    // 加载当前用户的记账类型
-    db.collection('account_type').where({
-      _openid: this.data.openid
-    }).get({
-      success: res => {
-        this.setData({
-          accountTypeArray: res.data[0],
-          multiArray: [res.data[0].level1, res.data[0].level2[0]],
-          accountType1: res.data[0].level1[0],
-          accountType2: res.data[0].level2[0][0],
-        })
-        console.log('[数据库] [查询记录] 成功: ', res)
-      },
-      fail: err => {
-        wx.showToast({
-          icon: 'none',
-          title: '查询记录失败'
-        })
-        console.error('[数据库] [查询记录] 失败：', err)
-      }
-    })
+    this.queryAccountType("支出");
   },
   onReady: function() {
+    var datetime = new Date();
+    var date = datetime.getFullYear() + "-" + (datetime.getMonth() + 1) + "-" + datetime.getDate();
+    var time = datetime.getHours() + ":" + datetime.getMinutes();
+    var startDate = (datetime.getFullYear() - 1) + "-" + (datetime.getMonth() + 1) + "-" + datetime.getDate();
+    var endDate = (datetime.getFullYear() + 1) + "-" + (datetime.getMonth() + 1) + "-" + datetime.getDate();
+    console.log("当前加载的日期：" + date);
+    console.log("当前加载的时间：" + time);
     this.setData({
+      date: date,
+      startDate: startDate,
+      endDate: endDate,
+      time: time,
       shouzhi: "支出",
       zhichuStyle: "background: #ADD8E6;color:#ffffff;",
       shouruStyle: "background: #ffffff;color:#ADD8E6;"
     })
+    //查找最近的记账记录
+    this.queryAccountRecord();
   },
   //点击收入或支出时触发，改变按钮颜色和收支类型
   clickShouZhi: function(e) {
@@ -67,20 +60,33 @@ Page({
         zhichuStyle: "background: #ffffff;color:#ADD8E6;"
       })
     }
+    this.queryAccountType(e.target.dataset.hi);
   },
-  //金额输入框失去焦点时触发，四舍五入保留两位小数
+  //金额输入框四舍五入保留两位小数
   jineInput: function(e) {
-    if (e.detail.value.toString().split(".").length==2){
+    if (e.detail.value.toString().split(".").length == 2) {
       if (e.detail.value.toString().split(".")[1].length > 2) {
         this.setData({
           jine: Math.floor(e.detail.value * 100) / 100
         })
       }
-    } 
+    }
     if (e.detail.value.toString().split(".").length > 2) {
       this.setData({
-        jine: parseFloat(e.detail.value.toString().split(".")[0] +"."+ e.detail.value.toString().split(".")[1])
+        jine: parseFloat(e.detail.value.toString().split(".")[0] + "." + e.detail.value.toString().split(".")[1])
       })
+    }
+  },
+  jineBlur: function(e) {
+    this.setData({
+      jine: e.detail.value * 1
+    })
+    if (e.detail.value.toString().split(".").length == 2) {
+      if (e.detail.value.toString().split(".")[1].length == 0) {
+        this.setData({
+          jine: Math.floor(e.detail.value * 100) / 100
+        })
+      }
     }
   },
   //转动收支类型选择器的事件
@@ -96,12 +102,38 @@ Page({
     this.setData(data);
   },
   //确认选择收支类型选择器的事件
-  bindMultiPickerChange:function(e){
+  bindMultiPickerChange: function(e) {
     this.setData({
       accountType1: this.data.accountTypeArray.level1[this.data.multiIndex[0]],
       accountType2: this.data.accountTypeArray.level2[this.data.multiIndex[0]][this.data.multiIndex[1]],
     })
   },
+  //确认选择日期
+  bindDateChange: function(e) {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  //确认选择时间
+  bindTimeChange: function(e) {
+    this.setData({
+      time: e.detail.value
+    })
+  },
+  //备注输入框输入
+  beizhuInput: function(e) {
+    if (e.detail.value.length > 100) {
+      this.setData({
+        beizhu: e.detail.value.substring(0, 100)
+      })
+    }
+  },
+  beizhuBlur: function(e) {
+    this.setData({
+      beizhu: e.detail.value
+    })
+  },
+  //保存数据
   onAdd: function() {
     const db = wx.cloud.database()
     var datas = {
@@ -110,7 +142,7 @@ Page({
       beizhu: this.data.beizhu,
       accountType1: this.data.accountType1,
       accountType2: this.data.accountType2,
-      date: this.data.data,
+      date: this.data.date,
       time: this.data.time,
       creatTime: new Date(),
       updateTime: new Date()
@@ -127,38 +159,64 @@ Page({
         wx.showToast({
           title: '保存成功',
         })
-        console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id);
+        console.log('新增一条记账成功，记录 _id: ', res._id);
+        this.queryAccountRecord();
       },
       fail: err => {
         wx.showToast({
           icon: 'none',
           title: '新增记录失败'
         })
-        console.error('[数据库] [新增记录] 失败：', err)
+        console.error('新增一条记账失败：', err)
       }
     })
   },
-
-  onQuery: function() {
+  queryAccountType: function(shouzhi) {
     const db = wx.cloud.database()
-    // 查询当前用户所有的 counters
+    // 加载当前用户的记账类型
     db.collection('account_type').where({
-      _openid: this.data.openid
+      _openid: this.data.openid,
+      accountType: shouzhi
     }).get({
       success: res => {
         this.setData({
           accountTypeArray: res.data[0],
-          multiArray: [res.data[0].level1, res.data[0].level2[0]]
+          multiArray: [res.data[0].level1, res.data[0].level2[0]],
+          accountType1: res.data[0].level1[0],
+          accountType2: res.data[0].level2[0][0],
         })
-        console.log('[数据库] [查询记录] 成功: ', res)
+        console.log('查找当前记账类型成功: ', res)
       },
       fail: err => {
         wx.showToast({
           icon: 'none',
           title: '查询记录失败'
         })
-        console.error('[数据库] [查询记录] 失败：', err)
+        console.error('查找当前记账类型失败：', err)
       }
     })
   },
+  queryAccountRecord:function(){
+    const db = wx.cloud.database()
+    db.collection('accounts').where({
+      _openid: this.data.openid
+    })
+      .orderBy('creatTime', 'desc')
+      .limit(5)
+      .get({
+        success: res => {
+          this.setData({
+            queryResult: res.data
+          })
+          console.log('查找新进的记账记录成功: ', res);
+        },
+        fail: err => {
+          wx.showToast({
+            icon: 'none',
+            title: '查询记录失败'
+          })
+          console.error('查找新进的记账记录失败：', err)
+        }
+      })
+  }
 })
