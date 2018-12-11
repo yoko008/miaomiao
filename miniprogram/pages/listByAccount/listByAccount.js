0df70633d3bfe1f5bd181f7a4370a4160e55561b
bugmaker <289115128@qq.com>
2018-12-11 18:30:56 +0800

    将按分类查看的数据显示在页面上

--- miniprogram/pages/listByAccount/listByAccount.js
+++ miniprogram/pages/listByAccount/listByAccount.js
@@ -9,13 +9,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    res:null //结果集
+    res:null ,//结果集
+    zhichuTotal:0,
+    shouruTotal:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    const this_ = this;
     wx.cloud.callFunction({
       // 需调用的云函数名
       name: 'getListByAccounts',
@@ -29,7 +32,6 @@ Page({
         var resArr = new Array();
         var arrLength = 0;
         for(var i=0;i<res.result.data.length;i++){
-          console.log(res.result.data[i].jine);
           var itHas = false;
           for(var j=0;j<resArr.length;j++){
             if (resArr[j].acc1 == res.result.data[i].accountType1 && resArr[j].shouzhi == res.result.data[i].shouzhi){
@@ -46,7 +48,24 @@ Page({
             resArr.push(obj);
           }
         }
+        var zhichuTotal =0;
+        var shouruTotal =0;
+        for(var i=0;i<resArr.length;i++){
+          resArr[i].jineStr = this_.numberFormat(resArr[i].jine,2,".",",");
+          if (resArr[i].shouzhi=="支出"){
+            zhichuTotal += resArr[i].jine;
+          }
+          if (resArr[i].shouzhi == "收入") {
+            shouruTotal += resArr[i].jine;
+          }
+        }
+        console.log("转换后的数组：");
         console.log(resArr);
+        this_.setData({
+          res: resArr,
+          zhichuTotal:zhichuTotal,
+          shouruTotal:shouruTotal
+        })
       },
       complete: console.log
     })
@@ -101,14 +120,36 @@ Page({
 
   },
 
-  contains:function(arr, obj) {
-    var i = arr.length;
-    while (i--) {
-      if (arr[i] == obj) {
-        return true;
-      }
+  numberFormat: function (number, decimals, dec_point, thousands_sep) {
+    /*
+     * 参数说明：
+     * number：要格式化的数字
+     * decimals：保留几位小数
+     * dec_point：小数点符号
+     * thousands_sep：千分位符号
+     * */
+    number = (number + '').replace(/[^0-9+-Ee.]/g, '');
+    var n = !isFinite(+number) ? 0 : +number,
+      prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+      sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+      dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+      s = '',
+      toFixedFix = function (n, prec) {
+        var k = Math.pow(10, prec);
+        return '' + Math.ceil(n * k) / k;
+      };
+
+    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+    var re = /(-?\d+)(\d{3})/;
+    while (re.test(s[0])) {
+      s[0] = s[0].replace(re, "$1" + sep + "$2");
     }
-    return false;
+
+    if ((s[1] || '').length < prec) {
+      s[1] = s[1] || '';
+      s[1] += new Array(prec - s[1].length + 1).join('0');
+    }
+    return s.join(dec);
   }
 
 })
