cfab7ee0e49ed7ac861dfbcf34eb3bd80c113b59
BugMakerZD <zhaodi@instreet.cn>
2019-01-01 22:08:21 +0800

    明天再写

--- miniprogram/pages/setTimeOut/add/add.js
+++ miniprogram/pages/setTimeOut/add/add.js
@@ -1,74 +1,66 @@
-// miniprogram/pages/setTimeOut/setTimeOut.js
+// miniprogram/pages/setTimeOut/add/add.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    left: 0
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
-  //点击右上角开启修改新增页面
-  touchAdd: function() {
-    wx.navigateTo({
-      url: './add/add',
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  },
+
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
